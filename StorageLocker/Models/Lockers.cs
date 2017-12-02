e598cb5a81c4e4904645361ddc378f7211b1e612
ZEUS\3lmo <pschwartz914@gmail.com>
2017-12-03 09:33:49 +1000

    Initial Commit

---
+++ StorageLocker/Models/Lockers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageLocker.Models
+{
+    public class Lockers
+    {
+        public enum LockerSize {Small, Medium, Large};
+        public int LockerId { get; set; }
+        public bool IsRented { get; set; }
+        public LockerSize Size { get; set; }
+        public Ticket TicketId { get; set; }
+
+
+
+    }
+}
---
+++ StorageLocker/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageLocker.Models
+{
+    public class Ticket
+    {
+        public int TicketId { get; set; } //PK
+        public int LockerId { get; set; } //FK
+        public bool StillCurrent { get; set; }
+        public int Archived { get; set; }
+
+    }
+}
